1df98cdfae1be487bdb3342f47fae5ea59b39c20
nigel.andam <nigel.andam@adec-innovations.com>
2022-05-24 18:48:46 +0800

    Get with Route Params

--- src/server.js
+++ src/server.js
@@ -8,5 +8,6 @@ app.use(bodyParser.json());
 //Routes
 app.get("/hello", (req, res) => res.send("Hello"));
 app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}`))
+app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`));
 
 app.listen(8000, () => console.log("Listening on Port 8000"))
