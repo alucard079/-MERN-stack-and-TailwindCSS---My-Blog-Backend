db55e76b45cff5699db622881e6412d3a88a8607
nigel.andam <nigel.andam@adec-innovations.com>
2022-05-24 18:11:40 +0800

    Added Post method

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
+app.use(bodyParser.json());
+
+//Routes
 app.get("/hello", (req, res) => res.send("Hello"));
+app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}`))
 
 app.listen(8000, () => console.log("Listening on Port 8000"))
